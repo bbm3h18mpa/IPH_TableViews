5570c6503afa4eda91daf07a00581d6e49d6e366
Florian <>
2021-11-09 09:05:40 +0100

    Add TableView with basic functions

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -9,8 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var games = ["Gta V", "Minecraft", "CS:GO"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+        title = "Spiele"
     }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = games[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
 }
